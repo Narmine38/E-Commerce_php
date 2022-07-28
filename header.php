fb7247c0195238a4f0e2459b3343317669b4d722
Cédric ATZORI <cedric.atzori@Digital-Grenoble.local>
2022-07-28 11:42:19 +0200

    add file header

---
+++ header.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="https://www.linkedin.com/in/c%C3%A9dric-atzori-686119229/">
+                        <p class=" fs-4 fw-bold text-dark  ">Cedric.A</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <p class="fs-5 fw-bold text-dark  ">Cv</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="#">
+                        <p class="fs-5 fw-bold text-dark">Promo</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
