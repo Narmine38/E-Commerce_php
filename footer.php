a7b737516c8e13d019224b6288b77475e00d3e7e
Cédric ATZORI <cedric.atzori@Digital-Grenoble.local>
2022-07-28 11:42:57 +0200

    add file footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!-- footer -->
+<div class="container-fluid  d-flex  align-items-center">
+    <div class="col-md-8 col-sm-8">
+        <p class="copyright-text"> Copyright &copy; 2022 All Rights Reserved by
+            <a href="#">Cédric.A</a>.
+        </p>
+
+    </div>
+
+
+</div>
+<!-- footer -->
+
+
+
+
+
+
