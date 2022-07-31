4ceb08e8021f8e6a8237f9d9836a4f03cf60dff5
Cédric.A <cedric.atzori@le-campus-numerique.fr>
2022-07-31 22:34:35 +0200

    New Header

--- header.php
+++ header.php
@@ -1,25 +1,39 @@
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">C.A</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Livres</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Films</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Goodies</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
+
+
+<nav class="navbar navbar-expand-lg  bg-dark col-12" >
+    <div class=" col-3 ">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active text-white" aria-current="page" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Bon plan</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Catégorie
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Livre</a></li>
+                    <li><a class="dropdown-item" href="#">Filme</a></li>
+                    <li><a class="dropdown-item" href="#">Edition Collector</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Produits Dérivés</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled text-white">Disabled</a>
+            </li>
+        </ul>
+    </div>
+    <div class=" col-6 d-flex justify-content-center ">
+        <img src="img/HerosMarkets.png" width="730" height="200" class="img-fluid">
+    </div>
+    <div class=" col-3 d-flex justify-content-center">
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light text-white" type="submit">Search</button>
+        </form>
     </div>
 </nav>
+
